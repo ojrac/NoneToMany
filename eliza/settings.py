a7655323369da4c2c3f9e8fc414e2176021f7308
Eddie Hebert <edhebert@gmail.com>
2011-03-31 21:20:48 -0400

    Removes hardcode path to template directory.

--- eliza/settings.py
+++ eliza/settings.py
@@ -87,7 +87,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/var/www/django/eliza/templates",
     os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
