f0157191dc332828a3260a36d3f283406ef75b18
Eddie Hebert <edhebert@gmail.com>
2011-03-31 20:35:36 -0400

    Prevents us from uploading the sqlite directory on the live server.

--- deploy.py
+++ deploy.py
@@ -3,4 +3,6 @@
 
 from subprocess import call
 
+call(["mv", "eliza/eliza.sqlite", "eliza/eliza.sqlite.tmp"])
 call(["rsync", "-avz", ".", "eliza.iad01.hubspot-networks.net:/usr/share/hubspot/webapps/eliza"])
+call(["mv", "eliza/eliza.sqlite.tmp", "eliza/eliza.sqlite"])
